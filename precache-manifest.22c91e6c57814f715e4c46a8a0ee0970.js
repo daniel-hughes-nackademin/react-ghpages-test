b15c0b89e549e84b6ae8653e19b1ea1fc1f7e564
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2020-09-09 01:05:58 +0200

    Updates

---
+++ precache-manifest.22c91e6c57814f715e4c46a8a0ee0970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeeba9dd1d6c6fa9aafb2fcf907da330",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cb6d0aa28fefe7fff28a",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12e2004879bcfaf29590",
+    "url": "/my-app/static/js/2.a65cfecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a65cfecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6d0aa28fefe7fff28a",
+    "url": "/my-app/static/js/main.f20ff698.chunk.js"
+  },
+  {
+    "revision": "acdb88013445f4a0e93b",
+    "url": "/my-app/static/js/runtime-main.854e3755.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
